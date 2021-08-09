133698202b0c6d2a52b8176bcc473384425795b6
7sodikov <78784540+7sodikov@users.noreply.github.com>
2021-08-09 22:06:51 +0500

    first commit

--- URLShortner/ContentView.swift
+++ URLShortner/ContentView.swift
@@ -8,10 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var text = ""
+    @StateObject var viewModel = ViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView(.vertical) {
+                header()
+                
+                ForEach(viewModel.models, id: \.self) { model in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("https://1pt.co/"+model.short)
+                            Text(model.long)
+                        }
+                        Spacer()
+                    }
+                    .padding()
+                    .onTapGesture {
+                        guard let url = URL(string: "https://1pt.co/"+model.short) else {
+                            return
+                        }
+                        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                    }
+                }
+                    
+            }
+            .navigationTitle("SHORT URLs")
+        }
     }
+    
+    @ViewBuilder
+    func header() -> some View {
+        VStack {
+            Text("Enter URL to be shortened")
+                .bold()
+                .font(.system(size: 26))
+                .foregroundColor(Color.white)
+                .padding()
+            
+            TextField("URL...", text: $text)
+                .padding()
+                .autocapitalization(.none)
+                .background(Color.white)
+                .cornerRadius(8)
+                .padding()
+            
+            Button(action: {
+                // make api call
+                guard !text.isEmpty else {
+                    return
+                }
+                viewModel.submit(urlString: text)
+                
+                text = ""
+                
+                
+            },
+                   label: {
+                Text("SUBMIT")
+                    .bold()
+                    .foregroundColor(Color.white)
+                    .frame(width: 240, height: 50)
+                    .background(Color.pink)
+                    .cornerRadius(8)
+                    .padding()
+            })
+        
+        }
+        .frame(width: UIScreen.main.bounds.width,
+               height: UIScreen.main.bounds.width)
+        .background(Color.blue)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ URLShortner/ViewModel.swift
@@ -0,0 +1,62 @@
+//
+//  ViewModel.swift
+//  URLShortner
+//
+//  Created by Utkirjon on 09/08/21.
+//
+
+import Foundation
+
+struct Model: Hashable {
+    let long: String
+    let short: String
+}
+
+class ViewModel: ObservableObject {
+    @Published var models = [Model]()
+    
+    func submit(urlString: String) {
+        guard URL(string: urlString) != nil else {
+            return
+        }
+        
+        // API Call
+        
+        guard let apiUrl = URL(string: "https://api.1pt.co/addURL?long="+urlString.lowercased()) else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: apiUrl) { [ weak self ] data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            // Convert data to JSON
+            
+            do {
+                let result = try JSONDecoder().decode(APIResponse.self, from: data)
+                print("RESULT: \(result)")
+                let long = result.long
+                let short = result.short
+                DispatchQueue.main.async {
+                    self?.models.append(.init(long: long, short: short))
+                }
+                
+            }
+            catch {
+                print(error)
+            }
+            
+        }
+        task.resume()
+        
+    }
+    
+}
+
+struct APIResponse: Codable {
+    let status: Int
+    let message: String
+    let short: String
+    let long: String
+}
