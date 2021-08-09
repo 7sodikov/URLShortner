177adb0ab365eecaa992e0705cd116d55de5f82d
7sodikov <78784540+7sodikov@users.noreply.github.com>
2021-08-09 14:10:34 +0500

    Initial Commit

---
+++ URLShortner/URLShortnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLShortnerApp.swift
+//  URLShortner
+//
+//  Created by Utkirjon on 09/08/21.
+//
+
+import SwiftUI
+
+@main
+struct URLShortnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
